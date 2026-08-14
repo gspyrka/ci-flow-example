998d1bfd433bd4abd5d6ba8bc31d25c0f0d5549d
Grzegorz Spyrka <grzegorz.spyrka@firma.interia.pl>
2026-08-14 10:02:47 +0200

    init commit

---
+++ src/sample_api/main.py
@@ -0,0 +1,70 @@
+import logging
+import sys
+import json
+from datetime import datetime, timezone
+from fastapi import FastAPI, Request
+import uvicorn
+
+class JSONFormatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            "@timestamp": datetime.now(timezone.utc).isoformat(),
+            "log.level": record.levelname.lower(),
+            "message": record.getMessage(),
+            "service.name": "proste-api",
+            "logger.name": record.name,
+        }
+        if hasattr(record, "http_method"):
+            log_record["http.method"] = record.http_method
+        if hasattr(record, "http_path"):
+            log_record["url.path"] = record.http_path
+            
+        return json.dumps(log_record)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(JSONFormatter())
+
+logger = logging.getLogger("api-logger")
+logger.setLevel(logging.INFO)
+logger.handlers = [handler]
+logger.propagate = False
+
+app = FastAPI(title="Elastic JSON API", version="1.0")
+
+# def main():
+#     uvicorn.run("sample_api.main:app", host="0.0.0.0", port=8000, reload=True)
+
+# if __name__ == "__main__":
+#     main()
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    start_time = datetime.now(timezone.utc)
+    response = await call_next(request)
+    duration = (datetime.now(timezone.utc) - start_time).total_seconds()
+
+    logger.info(
+        f"HTTP request handled",
+        extra={
+            "http_method": request.method,
+            "http_path": request.url.path,
+            "http.status_code": response.status_code,
+            "event.duration": duration
+        }
+    )
+    return response
+
+@app.get("/")
+def read_root():
+    logger.info("Wywołano endpoint główny /")
+    return {"status": "ok", "message": "API działa z logami pod Elasticsearch!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    logger.info(f"Pobieranie przedmiotu o ID: {item_id}")
+    return {"item_id": item_id, "name": "Przykładowy przedmiot"}
+
+@app.get("/healtz")
+def read_item():
+    logger.info(f"Healtcheck")
+    return {""}
